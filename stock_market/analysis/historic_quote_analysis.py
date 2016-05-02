85cb9cb4b89b68aa5ba4d03cffa64151158e95ea
ryan coverick <rcoverick@gmail.com>
2016-05-01 22:04:02 -0500

    added additional calculations for historic analysis and added to master scraper to use multiprocessing for scraping and analyzing historic data.

--- stock_market/analysis/historic_quote_analysis.py
+++ stock_market/analysis/historic_quote_analysis.py
@@ -5,15 +5,23 @@ from statistics import mean, pstdev
 
 def analyze(symbol,lock):
     """returns a dict of statistics about the historic quotes for the symbol"""
-    quotes = sorted(scrape_historic_quotes(symbol), key=lambda x: x['Date'], reverse=True)
+    quotes = sorted(scrape_historic_quotes(symbol), key=lambda x: x['Date'], reverse=True)[1:30]
+    dips = [x['Open']-x['Low'] for x in quotes]
+    jumps = [x['High']-x['Open'] for x in quotes]
     analysis= {}
-    analysis['avg_day_high']= mean([x['High'] for x in quotes[:30]])
-    analysis['avg_day_low'] = mean([x['Low'] for x in quotes[:30]])
-    analysis['day_high_std_dev'] = pstdev([x['High'] for x in quotes[:30]])
-    analysis['day_low_std_dev'] = pstdev([x['Low'] for x in quotes[:30]])
-    analysis['avg_close'] = mean([x['Close'] for x in quotes[:30]])
-    analysis['close_std_dev'] = pstdev([x['Close'] for x in quotes[:30]])
-    print(analysis)
+    analysis['avg_day_high']= mean([x['High'] for x in quotes])
+    analysis['avg_day_low'] = mean([x['Low'] for x in quotes])
+    analysis['day_high_std_dev'] = pstdev([x['High'] for x in quotes])
+    analysis['day_low_std_dev'] = pstdev([x['Low'] for x in quotes])
+    analysis['avg_close'] = mean([x['Close'] for x in quotes])
+    analysis['close_std_dev'] = pstdev([x['Close'] for x in quotes])
+    analysis['avg_dip'] = mean(dips)
+    analysis['avg_jump'] = mean(jumps)
+    analysis['dip_std_dev'] = pstdev(dips,analysis['avg_dip'])
+    analysis['jump_std_dev'] = pstdev(jumps, analysis['avg_jump'])
+    most_recent = quotes[0]
+    analysis['target_entry_price'] = most_recent['Close'] - (analysis['avg_dip']-analysis['dip_std_dev'])
+    analysis['Symbol']=most_recent['Symbol']
     
 if __name__=='__main__':
-    analyze("team", None)
+    analyze("cfr", None)
--- stock_market/scrapers/master.py
+++ stock_market/scrapers/master.py
@@ -1,20 +1,41 @@
 from company_overviews import scrape_exchange_listings
 from scrape_historic_quotes import scrape_historic_quotes
+from historic_quote_analysis import analyze
+from csv import DictWriter
+from multiprocessing import Pool, Pipe
+import os
 
 
-
-
-
+def process_work(args):
+    company, conn = args
+    try:
+        analysis = analyze(company["Symbol"],None)
+        conn.send(analysis)
+    except Exception as e:
+        print("Error for "+company["Symbol"])
+        
 if __name__=="__main__":
+    analysis_results = []
     companies = scrape_exchange_listings()
-    print("Retrieved "+str(len(companies))+" companies from exchanges")
-    quotes = []
-    for company in companies:
-        print("Scraping quotes for "+company["Symbol"])
-        try:
-            historic_quotes= scrape_historic_quotes(company["Symbol"])
-            print("Found "+str(len(historic_quotes))+" quotes")
-            quotes.extend(historic_quotes)
-        except:
-            print("Failed getting quotes for "+company["Symbol"])
-    print(str(len(quotes))+" total historic quotes scraped")
+    #start new csv file
+    with open('analysis.csv','w') as csvfile:
+        fieldnames = ['Symbol', 'Date','avg_day_high',
+                     'avg_day_low',
+                     'day_high_std_dev',
+                      'day_low_std_dev',
+                     'avg_close',
+                      'close_std_dev',
+                     'avg_dip',
+                     'avg_jump',
+                     'dip_std_dev',
+                     'jump_std_dev',
+                     'target_entry_price' ]
+        writer = DictWriter(csvfile,fieldnames=fieldnames)
+        writer.writeheader()
+        parent, child = Pipe()
+        with Pool(os.cpu_count()) as pool:
+            pool.map(process_work, [(x,child) for x in companies])
+        
+        while parent.poll(20):
+            result = parent.recv()
+            writer.writerow(result)
