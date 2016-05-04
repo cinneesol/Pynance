e952687edd3ead2af5f405d2f55f1e71e3666537
ryan coverick <rcoverick@gmail.com>
2016-05-04 17:17:06 -0500

    added functionality to save results of master scrape/analysis to postgres database

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ mega-project containing python scripts and programs to handle all things finance
 Dependendent packages for stock market data scraping:
 lxml 3.4.4
 BeautifulSoup 4
-
+Pygresql 5.0
--- stock_market/scrapers/master.py
+++ stock_market/scrapers/master.py
@@ -1,11 +1,11 @@
 from company_overviews import scrape_exchange_listings
 from scrape_historic_quotes import scrape_historic_quotes
 from historic_quote_analysis import analyze
-from csv import DictWriter
-from multiprocessing import Process, Queue, Pool
-import os
-
-
+from multiprocessing import Pool
+import json
+from pg import DB
+import dbprops
+import time
 def process_work(company):
     try:
         quotes = scrape_historic_quotes(company["Symbol"])
@@ -17,29 +17,21 @@ def process_work(company):
 
         
 if __name__=="__main__":
-    analysis_results = []
     companies = scrape_exchange_listings()
-    processes = 6
+    processes = 8
     results = []
     with Pool(processes) as p:
         results.extend(p.map(process_work, [x for x in companies]))
-    
+     
     print("Results: "+str(len(results)))
-    #start new csv file
-    with open('analysis.csv','w') as csvfile:
-        fieldnames = ['Symbol', 'Date','avg_day_high',
-                     'avg_day_low',
-                     'day_high_std_dev',
-                      'day_low_std_dev',
-                     'avg_close',
-                      'close_std_dev',
-                     'avg_dip',
-                     'avg_jump',
-                     'dip_std_dev',
-                     'jump_std_dev',
-                     'target_entry_price' ]
-        writer = DictWriter(csvfile,fieldnames=fieldnames)
-        writer.writeheader()
-        for r in results:
-            if r is not None:
-                writer.writerow(r)
+    try:
+        postgres = DB(host=dbprops.host, dbname=dbprops.database, 
+                      port=dbprops.port, user=dbprops.user, passwd=dbprops.password)
+        for result in results:
+            if result is not None:
+                result["Date"] = result["Date"].isoformat()
+                postgres.insert('historic_analytic', data=result)
+        postgres.commit()
+    finally:
+        postgres.close()
+    print("TIme to run main function: "+str(time.process_time()))
