11a9f6eb6844a4b728a6c5360a7a42ae7d77d690
ryan coverick <rcoverick@gmail.com>
2016-06-01 23:34:18 -0500

    fixed bug in find near target entry point script where percentage was being passed as string and not as a number

--- stock_market/find_near_target_entry_point.py
+++ stock_market/find_near_target_entry_point.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 
-def get_nearby_stocks(percent):
+def get_nearby_stocks(percent=0):
     db = sqlite3.connect("stockdata.db")
     db.row_factory = sqlite3.Row
     c = db.cursor()
@@ -16,20 +16,21 @@ def get_nearby_stocks(percent):
        avg_day_low,
        avg_close,
        avg_dip,
-       dip_std_dev
+       dip_std_dev,
+       (ABS(target_entry_price-last_close)/last_close) as DISTANCE
        FROM historic_analytic 
        WHERE date =(SELECT MAX(date) FROM historic_analytic) 
        AND target_entry_price < target_exit_price
        AND ABS(target_exit_price-target_entry_price)/target_entry_price > .015
-       AND ABS(target_entry_price-last_close)/last_close <= ?
+       AND DISTANCE <= ?
        AND close_slope > 0
        AND day_low_slope >0
        AND day_high_slope > 0
-       AND avg_volume>50000
+       AND avg_volume>1000000
        AND last_close>10
        AND avg_close <=volume_weighted_avg_close
        
-       """, (percent,))
+       """, (float(percent),))
     
     results = []
     for r in c.fetchall():
