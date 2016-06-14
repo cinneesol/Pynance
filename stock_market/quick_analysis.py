1ba79797985517db7077bd2b40600f9c6caafaef
ryan coverick <rcoverick@gmail.com>
2016-06-13 23:47:13 -0500

    fix quick analysis function to analyze option chain if possible

--- stock_market/quick_analysis.py
+++ stock_market/quick_analysis.py
@@ -3,14 +3,17 @@ from analysis.options_analysis import analyze_options
 from scrapers.investopedia import historic_quotes,option_chain
 import sys
 import json
-
+from pprint import pprint 
 
 if __name__=='__main__':
     symbol = input("Enter symbol: ")
     analysis = analyze(historic_quotes(symbol.lower())) 
     analysis['last_quote']['Date']=analysis['last_quote']['Date'].isoformat()
     analysis['Date']= analysis['Date'].isoformat()
-#     analysis['option_chain_analysis']=analyze_options(option_chain(symbol.lower()))
-    
-    print(json.dumps(analysis))
+    try:
+        analysis['option_chain_analysis']=analyze_options(option_chain(symbol.lower()))
+    except Exception as e:
+        print(str(e))
+        
+    pprint(json.dumps(analysis))
     sys.exit()
